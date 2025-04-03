6d4c83dcec89d653efe10cca5997f2089e63cd2f
Akaolisa Ejikeme <akaolisa.ejikeme@redtechlimited.com>
2025-04-03 19:38:36 +0100

    Added db configs

---
+++ database.py
@@ -0,0 +1,21 @@
+import os
+from typing import Annotated
+
+from dotenv import load_dotenv
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL=f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/fastapi_jwt_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    with SessionLocal() as session:
+        yield session
+
+SessionDep = Annotated[SessionLocal, Depends(get_db)]
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import secrets
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# token = secrets.token_hex(32)
+# print(token)
